4b07a1be809ac317af94f2a709b460aa94da9f8f
DnsTvg <denys.tovhin@hneu.net>
2024-03-31 19:27:50 +0300

    4 Лаба

--- lab4/crud/forms.py
+++ lab4/crud/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from django.core.validators import MinValueValidator, MaxValueValidator
 
 from .models import Books, Author
+from .service.book import validate_book_form, validate_book_update_form
 
 
 class BookForm(forms.ModelForm):
@@ -16,3 +17,24 @@ class BookForm(forms.ModelForm):
     class Meta:
         model = Books
         fields = ['title', 'author', 'year']
+
+    def clean(self):
+        cleaned_data = super().clean()
+        errors = validate_book_form(cleaned_data)
+        if errors:
+            for field, message in errors.items():
+                self.add_error(field, message)
+
+        return cleaned_data
+
+    def clean_year(self):
+        year = self.cleaned_data.get('year')
+        title = self.cleaned_data.get('title')
+
+        if year is not None:  # Перевірка на наявність поля 'year'
+            errors = validate_book_update_form({'year': year,"title":title})
+            print("errors:",errors)
+            if errors:
+                raise forms.ValidationError(errors['year'])
+
+        return year
